20ae56e725311cd6b7310a59fe594097d5266957
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2022-01-07 20:39:32 +0300

    Initial Commit

---
+++ WeatherSUI/WeatherSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSUIApp.swift
+//  WeatherSUI
+//
+//  Created by Эдуард on 07.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
